a240db1ec78192c01da094f383cf346a15ebe50b
Nibbler73 <github.com@123.husic.de>
2016-09-02 21:44:23 +0200

    removing files from version control

--- config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_USERNAME', 'user');
-define('MYSQL_PASSWORD', 'password');
-define('MYSQL_DATABASE', 'database');
-
-define('MYSQL_TABLE', 'timetable');
